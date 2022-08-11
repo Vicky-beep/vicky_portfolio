8c2b3c14ae4ca2f16610e564dcf2bd6e625fdb07
victory balogun <Victorybalogun@gmail.com>
2022-08-11 09:22:52 +0100

    developed and styled the header component

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 const sections = document.querySelectorAll('.section');
 const secBtns = document.querySelectorAll('.controls');
 const secBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 function pageTransitions(){
     // Button click event
     for(let i = 0; i < secBtn.length; i++){
         secBtn[i].addEventListener('click', function(){ 
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].classList = currentBtn[0].className.replace(' active-btn', '');
+            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
     })
 }
-allSections.addEventListener('click', function(e){
+allSections.addEventListener('click', (e) =>{
     const id = e.target.dataset.id;
     if(id){
         // remove selected from the other btns
-        secBtns.forEach(function(btn){
-            btn.classList.remove('active-btn');
+        secBtns.forEach((btn) =>{
+            btn.classList.remove('active');
         })
+        e.target.classList.add('active');
+
+        // hide all sections
+       sections.forEach ((section) =>{
+           section.classList.remove('active');
+       })
+       const element = document.getElementById(id);
+       element.classList.add('active');
     }
+
 })
 }
-pageTransitions();
+ pageTransitions();
